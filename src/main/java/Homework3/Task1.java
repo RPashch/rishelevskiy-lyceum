0487a6939438c09ff0fced4fe5a8f8f08a8fbce9
Vitalik-ctrl <Vitalikvarhanik@gmail.com>
2020-11-06 15:15:10 +0200

    Homework3: Loops, random, algorithms.

--- src/main/java/Homework3/Task1.java
+++ src/main/java/Homework3/Task1.java
@@ -7,17 +7,17 @@ public class Task1 {
         int sum = 0;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int [] Arr = new int[n];
+        int [] arr = new int[n];
 
         System.out.println("Массив: ");
-        for (int i = 0; i < Arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             //Arr[i] =  0 + (int) (Math.random() * Integer.MAX_VALUE);
 
             //Случайные значения от 0 до 100
-            Arr[i] = 0 + (int) (Math.random() * 100);
+            arr[i] = 0 + (int) (Math.random() * 100);
 
-            System.out.print(Arr[i] + " ");
-            sum += Arr[i];
+            System.out.print(arr[i] + " ");
+            sum += arr[i];
         }
         System.out.println("\nСумма чисел массива = " + sum);
     }
