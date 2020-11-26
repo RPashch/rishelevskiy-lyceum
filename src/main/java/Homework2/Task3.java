b59580932aae0edb4b5c7b6d6d6f15a36b2382c6
Daria10 <72558793+Daria10@users.noreply.github.com>
2020-11-26 23:00:04 +0200

    Update Task3.java

--- src/main/java/Homework2/Task3.java
+++ src/main/java/Homework2/Task3.java
@@ -6,6 +6,7 @@ public class Task3 {
             for (int i = d; i > 0; i--) {
                 System.out.print("*" + " ");
             }
+            System.out.println(" ");
         }
     }
 }
