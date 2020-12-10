412692eca77efeb9b466a0c50deea4fe9dffb433
Daria10 <10dariask@gmail.com>
2020-12-10 21:48:17 +0200

    //

--- src/main/java/Homework2/Task3.java
+++ src/main/java/Homework2/Task3.java
@@ -4,8 +4,9 @@ public class Task3 {
     public static void main(String[] args) {
         for (int d = 8; d > 0; d--) {
             for (int i = d; i > 0; i--) {
-                System.out.print("*" + " ");
+                System.out.print("*");
             }
+            System.out.println(" ");
         }
     }
 }
